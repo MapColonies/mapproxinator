bcbd649f6261e8d04cc6b5e0e6b771fb70445dec
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2021-08-17 16:37:21 +0300

    fix: fixed naming and condition (#3)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import { container } from 'tsyringe';
 import { Application } from 'express';
-import { registerExternalValues } from './containerConfig';
 import { ServerBuilder } from './serverBuilder';
 
 function getApp(): Application {
-  registerExternalValues();
   const app = container.resolve(ServerBuilder).build();
   return app;
 }
--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,44 @@ import { container } from 'tsyringe';
 import { get } from 'config';
 import { DEFAULT_SERVER_PORT, Services } from './common/constants';
 import { getApp } from './app';
+import { IConfig } from './common/interfaces';
 import { Readiness } from './probe/readiness';
 import { PollManager } from './pollManager';
 import { Liveness } from './probe/liveness';
 import { Initializer } from './initializer';
+import { registerExternalValues } from './containerConfig';
 
 interface IServerConfig {
   port: string;
 }
 
-const serverConfig = get<IServerConfig>('server');
-const initMode = get<boolean>('initMode');
-const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
-
-const app = getApp();
-const logger = container.resolve<Logger>(Services.LOGGER);
-const pollManager = container.resolve(PollManager);
+registerExternalValues();
 const initializer = container.resolve(Initializer);
-const healthCheck = container.resolve(Liveness).probe;
-const readyCheck = container.resolve(Readiness).probe;
-const server = createTerminus(createServer(app), {
-  healthChecks: { '/liveness': healthCheck, '/readiness': readyCheck, onSignal: container.resolve('onSignal') },
-});
-server.listen(port, () => {
-  logger.info(`app started on port ${port}`);
-});
+const logger = container.resolve<Logger>(Services.LOGGER);
+const config = container.resolve<IConfig>(Services.CONFIG);
+const pollManager = container.resolve(PollManager);
+const initMode = config.get<boolean>('initMode');
+
+const startServer = (): void => {
+  const serverConfig = get<IServerConfig>('server');
+  const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
+  const app = getApp();
+  const healthCheck = container.resolve(Liveness).probe;
+  const readyCheck = container.resolve(Readiness).probe;
+  const server = createTerminus(createServer(app), {
+    healthChecks: { '/liveness': healthCheck, '/readiness': readyCheck, onSignal: container.resolve('onSignal') },
+  });
+
+  server.listen(port, () => {
+    logger.info(`app started on port ${port}`);
+  });
+};
 
 try {
   if (initMode) {
     void initializer.init();
   } else {
+    startServer();
     void pollManager.poll();
   }
 } catch (error) {
