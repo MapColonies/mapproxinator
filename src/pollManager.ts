66b5fe415d57600d8bb0b65adc1fd3c12589529f
ronenk1 <ronenk1@rnd-hub.com>
2023-12-12 17:07:49 +0200

    fix: linter

--- src/pollManager.ts
+++ src/pollManager.ts
@@ -24,10 +24,10 @@ export class PollManager {
       this.logger.info(`polling attempt`);
       if (!(await this.watcher.isUpdated())) {
         this.logger.debug('changes detected!');
-        
+
         this.logger.info('updating configurations');
         await this.configProvider.createOrUpdateConfigFile();
-        
+
         this.logger.info(`killing worker by graceful reload in uwsgi app`);
         await this.reloadApp();
         this.logger.info(`reload request was sent, app will be reloaded`);
