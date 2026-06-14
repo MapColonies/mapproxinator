b67ff82c35e0c87d8d9072cd0b5a94e7ac4656b3
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-06-14 14:50:43 +0300

    refactor: new jslogger

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -1,20 +1,25 @@
 import { container } from 'tsyringe';
 import { getOtelMixin } from '@map-colonies/tracing-utils';
 import { trace } from '@opentelemetry/api';
-import jsLogger, { LoggerOptions } from '@map-colonies/js-logger';
+import { jsLogger } from '@map-colonies/js-logger';
 import { Services } from './common/constants';
 import { tracing } from './common/tracing';
 import { IPollConfig } from './common/interfaces';
 import { getProvider } from './common/getProvider';
-function registerExternalValues(): void {
+
+export const registerExternalValues = async (): Promise<void> => {
   const configInstance = getConfig();
 
-  const loggerConfig = configInstance.get<LoggerOptions>('telemetry.logger');
+  const loggerConfig = configInstance.get('telemetry.logger');
+
+  const logger = await jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, mixin: getOtelMixin() });
+
   const provider = configInstance.get<string>('configProvider');
   const pollConfig = configInstance.get<IPollConfig>('poll');
   const fsConfig = configInstance.get(Services.FSCONFIG);
   const dbConfig = configInstance.get(Services.DBCONFIG);
   const s3Config = configInstance.get(Services.S3CONFIG);
+
   container.register(Services.CONFIG, { useValue: configInstance });
   container.register(Services.LOGGER, { useValue: logger });
   container.register(Services.FSCONFIG, { useValue: fsConfig });
@@ -32,6 +37,4 @@ function registerExternalValues(): void {
   container.register(Services.CONFIGPROVIDER, {
     useValue: getProvider(provider),
   });
-}
-
-export { registerExternalValues };
+};
--- tests/integration/testContainerConfig.ts
+++ tests/integration/testContainerConfig.ts
@@ -1,4 +1,4 @@
-import { container } from 'tsyringe';
+import { jsLogger } from '@map-colonies/js-logger';
 import { trace } from '@opentelemetry/api';
 import { container } from 'tsyringe';
 import { getConfig } from '../../src/common/config';
@@ -8,10 +8,12 @@ import type { IPollConfig } from '../../src/common/interfaces';
 
 export const registerTestValues = async (): Promise<void> => {
   const configInstance = getConfig();
+  const logger = await jsLogger({ enabled: false });
   const pollConfig = configInstance.get<IPollConfig>('poll');
   const provider = configInstance.get<string>('configProvider');
   const fsConfig = configInstance.get(Services.FSCONFIG);
   container.register(Services.CONFIG, { useValue: configInstance });
+  container.register(Services.LOGGER, { useValue: logger });
   container.register(Services.POLLCONFIG, { useValue: pollConfig });
   container.register(Services.FSCONFIG, { useValue: fsConfig });
   // if sdk is not initialized then getTracer returns a NoopTracer
@@ -21,6 +23,4 @@ export const registerTestValues = async (): Promise<void> => {
   container.register(Services.CONFIGPROVIDER, {
     useValue: getProvider(provider),
   });
-}
-
-export { registerTestValues };
+};
