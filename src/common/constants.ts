d445807172ac0136305e577a50728cd591d5d649
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-06-14 13:45:27 +0300

    refactor: extract service name from package.json

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,10 @@
+import { readPackageJsonSync } from '@map-colonies/read-pkg';
+
+export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
 export const DEFAULT_SERVER_PORT = 80;
 
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/];
-
-export const SERVICE_NAME = 'mapproxinator';
 
 export enum Services {
   LOGGER = 'ILogger',
