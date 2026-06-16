6c5e3cf39ae1729424a9a6dd51d8f72566b58f7f
vitaligi <54726763+vitaligi@users.noreply.github.com>
2026-06-16 14:28:31 +0300

    feat: generated openapi types

---
+++ src/openapi.d.ts
@@ -0,0 +1,106 @@
+/* eslint-disable */
+// This file was auto-generated. Do not edit manually.
+// To update, run the error generation script again.
+
+import type { TypedRequestHandlers as ImportedTypedRequestHandlers } from '@map-colonies/openapi-helpers/typedRequestHandler';
+export type paths = {
+  '/resourceName': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    /** gets the resource */
+    get: operations['getResourceName'];
+    put?: never;
+    /** creates a new record of type resource */
+    post: operations['createResource'];
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
+};
+export type webhooks = Record<string, never>;
+export type components = {
+  schemas: {
+    error: {
+      message: string;
+    };
+    resource: {
+      /** Format: int64 */
+      id: number;
+      name: string;
+      description: string;
+    };
+  };
+  responses: never;
+  parameters: never;
+  requestBodies: never;
+  headers: never;
+  pathItems: never;
+};
+export type $defs = Record<string, never>;
+export interface operations {
+  getResourceName: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      /** @description OK */
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          'application/json': components['schemas']['resource'];
+        };
+      };
+      /** @description Bad Request */
+      400: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          'application/json': components['schemas']['error'];
+        };
+      };
+    };
+  };
+  createResource: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      /** @description created */
+      201: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          'application/json': components['schemas']['resource'];
+        };
+      };
+      /** @description Bad Request */
+      400: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          'application/json': components['schemas']['error'];
+        };
+      };
+    };
+  };
+}
+export type TypedRequestHandlers = ImportedTypedRequestHandlers<paths, operations>;
